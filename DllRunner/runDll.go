9af1802029631df5aea1871ae8e5881255411bf2
James <jhchadwick@protonmail.com>
2020-07-25 01:45:34 +0100

    so DLL loads, function found, cant quite call function yet but getting there

--- DllRunner/runDll.go
+++ DllRunner/runDll.go
@@ -12,5 +12,14 @@ func main() {
 		log.Fatal(e)
 	}
 
-	fmt.Printf("%p", h)
+	rand, err := syscall.GetProcAddress(h, "cgoRandom")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	n, _, _ := syscall.Syscall9(uintptr(rand), 0, 2, 2, 2, 2, 2, 0, 0, 0, 0)
+
+	fmt.Printf("%x\n", h)
+	fmt.Printf("%x\n", &rand)
+	fmt.Printf("Function returns %d\n", n)
 }
